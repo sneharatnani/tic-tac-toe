e2c22dffb01e84f3e96fb930d7378157cc8c7424
sneharatnani <sneharatnani5@gmail.com>
2022-03-09 18:05:22 +0530

    added game function and changed design

--- script/main.js
+++ script/main.js
@@ -1,16 +1,25 @@
 const GameBoard = (function () {
     const optionBtn = document.querySelectorAll('.options button');
-    const openModal = () => document.querySelector('.modal').classList.remove('hide');
+    const startBtn = document.querySelector('.start-game');
+    // open modal to add names
     optionBtn.forEach((btn) => {
-        btn.addEventListener('click', openModal);
+        btn.addEventListener('click', () => {
+            document.querySelector('.modal').classList.remove('hide');
+        });
     });
-
-    function getWinner(boxes) {
-        if (boxes[0].textContent !== '' && boxes[1].textContent !== '' && boxes[2].textContent !== '' && boxes[3].textContent !== '' && boxes[4].textContent !== '' && boxes[5].textContent !== '' && boxes[6].textContent !== '' && boxes[7].textContent !== '' && boxes[8].textContent !== '') {
-            return `draw`;
-        }
+    // close model and open game body
+    startBtn.addEventListener('click', closeModal);
+    function closeModal() {
+        if (Players().player1 !== '' && Players().player2 !== '') {
+            document.querySelector('.modal').classList.add('hide');
+            document.querySelector('.options').classList.add('hide');
+            document.querySelector('.game').classList.remove('hide');
+        };
+    };
+    function getWinner() {
+        const boxes = document.querySelectorAll('.game p');
         // player one will win
-        else if (boxes[0].textContent === 'o' && boxes[1].textContent === 'o' && boxes[2].textContent === 'o') {
+        if (boxes[0].textContent === 'o' && boxes[1].textContent === 'o' && boxes[2].textContent === 'o') {
             return 'player1';
         }
         else if (boxes[0].textContent === 'o' && boxes[3].textContent === 'o' && boxes[6].textContent === 'o') {
@@ -59,6 +68,9 @@ const GameBoard = (function () {
         else if (boxes[3].textContent === 'x' && boxes[4].textContent === 'x' && boxes[5].textContent === 'x') {
             return 'player2';
         }
+        else if (boxes[0].textContent !== '' && boxes[1].textContent !== '' && boxes[2].textContent !== '' && boxes[3].textContent !== '' && boxes[4].textContent !== '' && boxes[5].textContent !== '' && boxes[6].textContent !== '' && boxes[7].textContent !== '' && boxes[8].textContent !== '') {
+            return `draw`;
+        }
     };
     return {
         getWinner
@@ -66,49 +78,77 @@ const GameBoard = (function () {
 })();
 
 
-const DisplayControl = (function (winner) {
-    const player1 = document.querySelector('#player1');
-    const player2 = document.querySelector('#player2');
-    // close model and open game body
-    function closeModal() {
-        if (player1.value !== '' && player2.value !== '') {
-            document.querySelector('.modal').classList.add('hide');
-            document.querySelector('.options').classList.add('hide');
-            document.querySelector('.game').classList.remove('hide');
-        };
-    };
-    document.querySelector('.start-game').addEventListener('click', closeModal);
-
-    // update player's name after every turn and declare winner
+const DisplayControl = (function () {
     const boxes = document.querySelectorAll('.game p');
     const display = document.querySelector('.display');
+    const restart = document.querySelector('.restart');
     let gameArr = ['o', 'x', 'o', 'x', 'o', 'x', 'o', 'x', 'o'];
     boxes.forEach((box) => {
         box.addEventListener('click', () => {
             if (box.textContent === '') {
                 box.textContent = gameArr.shift();
-                updateDisplay(box, boxes);
+                updateDisplay(box, GameBoard.getWinner());
             };
         });
     });
-    function updateDisplay(box) {
-        if (winner === 'draw') {
-            display.textContent = `It's a draw`;
-        }
-        else if (winner === 'player1') {
-            display.textContent = `winner is ${player1.value}`;
-        }
-        else if (winner === 'player2') {
-            display.textContent = `winner is ${player2.value}`;
-        }
-        else if (box.textContent === 'o') {
-            display.textContent = `${player2.value}'s turn`;
-        }
-        else {
-            display.textContent = `${player1.value}'s turn`;
-        }
+    // update player's name after every turn and declare winner
+    function updateDisplay(box, winner) {
+        switch (true) {
+            case winner === 'draw':
+                showResult(`It's a draw`)
+                break;
+
+            case winner === 'player1':
+                showResult(`winner is ${Players().player1}`)
+                break;
+
+            case winner === 'player2':
+                showResult(`winner is ${Players().player2}`)
+                break;
+
+            case box.textContent === 'o':
+                display.textContent = `${Players().player2}'s turn`;
+                break;
+
+            default:
+                display.textContent = `${Players().player1}'s turn`;
+        };
     };
-})(GameBoard.getWinner(boxes));
+    function showResult(str) {
+        display.textContent = str;
+        boxes.forEach((box) => {
+            box.style.pointerEvents = 'none';
+        });
+        restart.classList.remove('hide'); /* display restart button */
+    };
+
+    const restartGame = () => {
+        document.querySelector('.game').classList.add('hide');
+        document.querySelector('.display').textContent = '';
+        document.querySelector('.options').classList.remove('hide');
+        document.querySelector('#player1').value = '';
+        document.querySelector('#player2').value = '';
+        document.querySelectorAll('.game p').forEach((box) => {
+            box.textContent = '';
+            box.style.pointerEvents = 'all';
+        });
+        gameArr = ['o', 'x', 'o', 'x', 'o', 'x', 'o', 'x', 'o'];
+        restart.classList.add('hide');
+    };
+    restart.addEventListener('click', restartGame);
+})();
+
+function Players() {
+    const player1 = document.querySelector('#player1').value;
+    const player2 = document.querySelector('#player2').value;
+    return {
+        player1,
+        player2
+    };
+}
+
+
+
 
 
 
