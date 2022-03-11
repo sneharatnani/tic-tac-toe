3ec6ab357fcbdf7811f709190c9bcdea1df675a5
sneharatnani <sneharatnani5@gmail.com>
2022-03-11 17:37:59 +0530

    changed some functions and organized them

--- script/main.js
+++ script/main.js
@@ -1,158 +1,3 @@
-const GameBoard = (function () {
-    const restart = document.querySelector('.restart');
-    const modal = document.querySelector('.modal');
-    const overlay = document.querySelector('.overlay');
-    const openModal = () => {
-        modal.classList.remove('hide');
-        overlay.classList.remove('hide');
-    };
-    function closeModal() {
-        if (Players().player1 !== '' && Players().player2 !== '') {
-            modal.classList.add('hide');
-            overlay.classList.add('hide');
-            document.querySelector('.options').classList.add('hide');
-            document.querySelector('.game').classList.remove('hide');
-        };
-    };
-    function getWinner() {
-        const boxes = document.querySelectorAll('.game p');
-        // player one will win
-        if (boxes[0].textContent === 'o' && boxes[1].textContent === 'o' && boxes[2].textContent === 'o') {
-            return 'player1';
-        }
-        else if (boxes[0].textContent === 'o' && boxes[3].textContent === 'o' && boxes[6].textContent === 'o') {
-            return 'player1';
-        }
-        else if (boxes[6].textContent === 'o' && boxes[7].textContent === 'o' && boxes[8].textContent === 'o') {
-            return 'player1';
-        }
-        else if (boxes[2].textContent === 'o' && boxes[5].textContent === 'o' && boxes[8].textContent === 'o') {
-            return 'player1';
-        }
-        else if (boxes[0].textContent === 'o' && boxes[4].textContent === 'o' && boxes[8].textContent === 'o') {
-            return 'player1';
-        }
-        else if (boxes[2].textContent === 'o' && boxes[4].textContent === 'o' && boxes[6].textContent === 'o') {
-            return 'player1';
-        }
-        else if (boxes[1].textContent === 'o' && boxes[4].textContent === 'o' && boxes[7].textContent === 'o') {
-            return 'player1';
-        }
-        else if (boxes[3].textContent === 'o' && boxes[4].textContent === 'o' && boxes[5].textContent === 'o') {
-            return 'player1';
-        }
-        // player two will win
-        else if (boxes[0].textContent === 'x' && boxes[1].textContent === 'x' && boxes[2].textContent === 'x') {
-            return 'player2';
-        }
-        else if (boxes[0].textContent === 'x' && boxes[3].textContent === 'x' && boxes[6].textContent === 'x') {
-            return 'player2';
-        }
-        else if (boxes[6].textContent === 'x' && boxes[7].textContent === 'x' && boxes[8].textContent === 'x') {
-            return 'player2';
-        }
-        else if (boxes[2].textContent === 'x' && boxes[5].textContent === 'x' && boxes[8].textContent === 'x') {
-            return 'player2';
-        }
-        else if (boxes[0].textContent === 'x' && boxes[4].textContent === 'x' && boxes[8].textContent === 'x') {
-            return 'player2';
-        }
-        else if (boxes[2].textContent === 'x' && boxes[4].textContent === 'x' && boxes[6].textContent === 'x') {
-            return 'player2';
-        }
-        else if (boxes[1].textContent === 'x' && boxes[4].textContent === 'x' && boxes[7].textContent === 'x') {
-            return 'player2';
-        }
-        else if (boxes[3].textContent === 'x' && boxes[4].textContent === 'x' && boxes[5].textContent === 'x') {
-            return 'player2';
-        }
-        else if (boxes[0].textContent !== '' && boxes[1].textContent !== '' && boxes[2].textContent !== '' && boxes[3].textContent !== '' && boxes[4].textContent !== '' && boxes[5].textContent !== '' && boxes[6].textContent !== '' && boxes[7].textContent !== '' && boxes[8].textContent !== '') {
-            return `draw`;
-        }
-    };
-    restart.addEventListener('click', () => DisplayControl.restartGame());
-    return {
-        getWinner,
-        openModal,
-        closeModal,
-    };
-})();
-
-
-const DisplayControl = (function () {
-    // open modal to add names
-    const optionBtn = document.querySelectorAll('.options button');
-    optionBtn.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            GameBoard.openModal();
-        });
-    });
-    // close model and open game body
-    const startBtn = document.querySelector('.start-game');
-    startBtn.addEventListener('click', () => {
-        GameBoard.closeModal();
-    });
-    const boxes = document.querySelectorAll('.game p');
-    const display = document.querySelector('.display');
-    const restart = document.querySelector('.restart');
-    let gameArr = ['o', 'x', 'o', 'x', 'o', 'x', 'o', 'x', 'o'];
-    boxes.forEach((box) => {
-        box.addEventListener('click', () => {
-            if (box.textContent === '') {
-                box.textContent = gameArr.shift();
-                updateDisplay(box, GameBoard.getWinner());
-            };
-        });
-    });
-    // update player's name after every turn and declare winner
-    function updateDisplay(box, winner) {
-        switch (true) {
-            case winner === 'draw':
-                showResult(`It's a draw`)
-                break;
-
-            case winner === 'player1':
-                showResult(`winner is ${Players().player1}`)
-                break;
-
-            case winner === 'player2':
-                showResult(`winner is ${Players().player2}`)
-                break;
-
-            case box.textContent === 'o':
-                display.textContent = `${Players().player2}'s turn`;
-                break;
-
-            default:
-                display.textContent = `${Players().player1}'s turn`;
-        };
-    };
-    function showResult(str) {
-        display.textContent = str;
-        boxes.forEach((box) => {
-            box.style.pointerEvents = 'none';
-        });
-        restart.classList.remove('hide'); /* display restart button */
-    };
-
-    const restartGame = () => {
-        document.querySelector('.game').classList.add('hide');
-        document.querySelector('.display').textContent = '';
-        document.querySelector('.options').classList.remove('hide');
-        document.querySelector('#player1').value = '';
-        document.querySelector('#player2').value = '';
-        document.querySelectorAll('.game p').forEach((box) => {
-            box.textContent = '';
-            box.style.pointerEvents = 'all';
-        });
-        gameArr = ['o', 'x', 'o', 'x', 'o', 'x', 'o', 'x', 'o'];
-        restart.classList.add('hide');
-    };
-    return {
-        restartGame,
-    };
-})();
-
 function Players() {
     const player1 = document.querySelector('#player1').value;
     const player2 = document.querySelector('#player2').value;
@@ -162,12 +7,227 @@ function Players() {
     };
 }
 
+const GameBoard = (function () {
+    // DOM
+    const gameField = document.querySelectorAll('.game p');
+    let gameArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    // get index from arr
+    function getIndex() {
+        return gameArr.shift();
+    };
+
+    // set new arr after restart
+    function setGameArr() {
+        gameArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    };
+
+    // return sign according to index
+    function getSign(index) {
+        if (index === 2 || index === 4 || index === 6 || index === 8) {
+            return 'x';
+        }
+        else {
+            return 'o';
+        }
+    };
+
+    function _checkWinner(fields, sign) {
+        return gameField[fields[0]].textContent === sign && gameField[fields[1]].textContent === sign && gameField[fields[2]].textContent === sign;
+    };
+    // check for winner
+    function getWinner() {
+        if (_checkWinner([0, 1, 2], 'o') || _checkWinner([0, 3, 6], 'o') || _checkWinner([6, 7, 8], 'o') || _checkWinner([2, 5, 8], 'o') || _checkWinner([0, 4, 8], 'o') || _checkWinner([2, 4, 6], 'o') || _checkWinner([1, 4, 7], 'o') || _checkWinner([3, 4, 5], 'o')) {
+            return 1
+        }
+
+        else if ((_checkWinner([0, 1, 2], 'x') || _checkWinner([0, 3, 6], 'x') || _checkWinner([6, 7, 8], 'x') || _checkWinner([2, 5, 8], 'x') || _checkWinner([0, 4, 8], 'x') || _checkWinner([2, 4, 6], 'x') || _checkWinner([1, 4, 7], 'x') || _checkWinner([3, 4, 5], 'x'))) {
+            return 2
+        }
+    };
+
+    // check for draw
+    function checkDraw(fields) {
+        for (let i = 0; i < 9; i++) {
+            if (fields[i].textContent === '') {
+                return false
+            };
+        };
+        return true;
+    };
+
+    return {
+        setGameArr,
+        getSign,
+        getIndex,
+        checkDraw,
+        getWinner,
+    };
+})();
+
+const displayControl = (function () {
+    // DOM 
+    const restart = document.querySelector('.restart');
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
+    const options = document.querySelector('.options');
+    const gameBody = document.querySelector('.game');
+    const gameSelectBtn = document.querySelectorAll('.options button');
+    const startBtn = document.querySelector('.start-game');
+    const display = document.querySelector('.display');
+    const gameField = document.querySelectorAll('.game p');
+
+    function _openModal() { /* open modal to add player names */
+        gameControl.showEle(modal);
+        gameControl.showEle(overlay);
+    };
+
+    function _closeModal() {
+        if (Players().player1 !== '' && Players().player2 !== '') {
+            gameControl.hideEle(modal);
+            gameControl.hideEle(overlay);
+        };
+    };
+    // close choosing options
+    function _closeOptions() {
+        gameControl.hideEle(options);
+    };
+
+    // open game choosing options
+    function openOptions() {
+        gameControl.showEle(options);
+    }
+    // open game body
+    function openGame() {
+        gameControl.showEle(gameBody);
+    };
+
+    // close game body
+    function closeGame() {
+        gameControl.hideEle(gameBody);
+    };
+
+    // clear game fields
+    function clearFields() {
+        gameField.forEach((field) => addText(field, ''));
+    }
+
+    // add text content to elements
+    function addText(ele, text) {
+        return ele.textContent = text;
+    };
+
+    // show player one's turn by default
+    function showDefault() {
+        addText(display, `${Players().player1}'s turn`);
+    };
+
+    // show player's turn
+    function showTurn(sign) {
+        if (sign === 'o') {
+            addText(display, `${Players().player2}'s turn`);
+        }
+        else {
+            showDefault();
+        }
+    };
+
+    // display winner, stop clicks, display restart
+    function displayWinner() {
+        if (GameBoard.getWinner() === 1) {
+            addText(display, `winner is ${Players().player1}`);
+            gameControl.stopClicks();/* stop clicks */
+            gameControl.showEle(restart);/* display restart btn */
+        }
+        else if (GameBoard.getWinner() === 2) {
+            addText(display, `winner is ${Players().player2}`);
+            gameControl.stopClicks();
+            gameControl.showEle(restart);
+        }
+        else if (GameBoard.checkDraw(gameField)) {
+            addText(display, `it's a draw`);
+            gameControl.showEle(restart);
+        }
+    };
+
+    // events
+    gameSelectBtn.forEach((btn) => {
+        btn.addEventListener('click', _openModal);
+    });
+
+    startBtn.addEventListener('click', () => {
+        if (Players().player1 !== '' && Players().player2 !== '')/* check for filled inputs */ {
+            _closeModal();
+            _closeOptions();
+            openGame();
+            showDefault();
+        };
+    });
+
+    return {
+        closeGame,
+        openOptions,
+        addText,
+        showTurn,
+        displayWinner,
+        clearFields,
+        openGame,
+        showDefault
+    };
+})();
 
 
+const gameControl = (function () {
+    // DOM
+    const gameField = document.querySelectorAll('.game p');
+    const restart = document.querySelector('.restart');
 
+    // to hide elements
+    function hideEle(ele) {
+        ele.classList.add('hide');
+    };
 
+    // to show elements
+    function showEle(ele) {
+        ele.classList.remove('hide');
+    };
 
+    // stop click events fields
+    function stopClicks() {
+        gameField.forEach((field) => field.style.pointerEvents = 'none');
+    };
 
+    // start click events on fields
+    function _startClicks() {
+        gameField.forEach((field) => field.style.pointerEvents = 'all');
+    }
 
+    // restart function
+    function _restartGame() {
+        displayControl.closeGame();/* close game */
+        displayControl.clearFields();/* clear fields */
+        GameBoard.setGameArr();/* set new gameArr */
+        _startClicks();
+        displayControl.showDefault();
+        displayControl.openGame();/* open game body to start again */
+    };
 
+    // events
+    gameField.forEach((field) => {
+        field.addEventListener('click', () => {
+            if (field.textContent === '') /* check for empaty field */ {
+                displayControl.addText(field, GameBoard.getSign(GameBoard.getIndex()));
+                displayControl.showTurn(field.textContent);
+                displayControl.displayWinner();
+            }
+        })
+    });
+
+    restart.addEventListener('click', _restartGame);
+    return {
+        hideEle,
+        showEle,
+        stopClicks,
+    };
+})();
 
